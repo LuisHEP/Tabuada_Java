b4aad30c78b56829d253470292bd1f0b4d432a2b
LuisHEP <luis.21.paladino@gmail.com>
2024-05-06 15:29:23 -0300

    Tabuada em Java

---
+++ src/Tabuada.java
@@ -0,0 +1,10 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        for (int multiplicador = 1; multiplicador <= 10; multiplicador++) {
+            System.out.println("Tabuada do " + multiplicador);
+            for (int contador = 0; contador <= 10; contador++) {
+                System.out.println(multiplicador + " x " + contador + " = " + multiplicador*contador);
+            }
+        }
+    }
+}
